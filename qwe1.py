2de49a3c4cfef550cc65247bad9b079de0d014a3
CookiePenguin <zakhar.maksimkin@gmail.com>
2021-11-22 19:01:13 +0300

    Всё готово?

---
+++ qwe1.py
@@ -0,0 +1,69 @@
+import sys
+from random import choice
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 800)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(290, 640, 231, 111))
+        font = QtGui.QFont()
+        font.setPointSize(36)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Тыкай и будут появляться кружочки МАГИЯ)"))
+        self.pushButton.setText(_translate("MainWindow", "ТЫКАЙ!"))
+
+
+class Example(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.setStyleSheet("background-color: #FFFF00")
+        self.pushButton.clicked.connect(self.qwa)
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+
+    def qwa(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw(self, qp):
+        self.coords_ = [choice(range(800)), choice(range(800))]
+        khe = choice(range(400))
+        qp.setBrush(QColor(choice(range(255)), choice(range(255)), choice(range(255))))
+        qp.drawEllipse(self.coords_[0], self.coords_[1], khe, khe)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
